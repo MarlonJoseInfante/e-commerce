59e4206e33b0e13d38c3b6e236ad0be5bc4b9d0a
= <=>
2023-01-17 18:52:34 -0300

    Se creo funcionalidad guardad producto, vista create, y logger para producto controller

--- src/main/java/com/practice/ecommerce/controller/ProductoController.java
+++ src/main/java/com/practice/ecommerce/controller/ProductoController.java
@@ -2,16 +2,41 @@
 
 package com.practice.ecommerce.controller;
 
+import com.practice.ecommerce.model.Producto;
+import com.practice.ecommerce.model.Usuario;
+import com.practice.ecommerce.service.ProductoService;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+    
+    private final Logger LOGGER= LoggerFactory.getLogger(ProductoController.class);
+    
+    @Autowired
+    private ProductoService productoService;
 
     @GetMapping("")
     public String show(){
         return "/productos/show";
+    }
+    
+    @GetMapping("/create")
+    public String create(){
+        return "/productos/create";
+    }
+    
+    @PostMapping("/save")
+    public String save(Producto producto){
+        LOGGER.info("Este es el producto{} ", producto);
+        Usuario u= new Usuario(1, "", "", "", "", "", "", "", null, null);
+        producto.setUsuario(u);
+        productoService.save(producto);
+        return "redirect:/productos";
     }
 }
