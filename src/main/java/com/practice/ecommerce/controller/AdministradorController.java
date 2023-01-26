f8c3e7500259d1d80a7d144a885f591f50edbf32
= <=>
2023-01-26 19:40:22 -0300

    Se implemento funcionalidad de cargar imagen y mostrar img para el admin

--- src/main/java/com/practice/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/practice/ecommerce/controller/AdministradorController.java
@@ -1,7 +1,12 @@
 
 package com.practice.ecommerce.controller;
 
+import com.practice.ecommerce.model.Producto;
+import com.practice.ecommerce.service.ProductoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/administrador")
 public class AdministradorController {
 
+    @Autowired
+    private ProductoService productoService;
+    
     @GetMapping("")
-    public String home(){
+    public String home(Model model){
+        List<Producto> productos= productoService.findAll();
+        model.addAttribute("productos", productos);
         return "/administrador/home";
     }
 }
--- src/main/java/com/practice/ecommerce/controller/ProductoController.java
+++ src/main/java/com/practice/ecommerce/controller/ProductoController.java
@@ -1,5 +1,3 @@
-
-
 package com.practice.ecommerce.controller;
 
 import com.practice.ecommerce.model.Producto;
@@ -22,69 +20,80 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
-    
-    private final Logger LOGGER= LoggerFactory.getLogger(ProductoController.class);
-    
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+
     @Autowired
     private ProductoService productoService;
-    
+
     @Autowired
     private UploadFileService upload;
 
     @GetMapping("")
-    public String show(Model model){
+    public String show(Model model) {
         model.addAttribute("productos", productoService.findAll());
         return "/productos/show";
     }
-    
+
     @GetMapping("/create")
-    public String create(){
+    public String create() {
         return "/productos/create";
     }
-    
+
     @PostMapping("/save")
-    public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException{
+    public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
         LOGGER.info("Este es el producto{} ", producto);
-        Usuario u= new Usuario(1, "", "", "", "", "", "", "", null, null);
+        Usuario u = new Usuario(1, "", "", "", "", "", "", "", null, null);
         producto.setUsuario(u);
         //imagen
-        if (producto.getId()== null) {// cuando se crea el producto
-            String nombreImagen= upload.saveImage(file);
+        if (producto.getId() == null) {// cuando se crea el producto
+            String nombreImagen = upload.saveImage(file);
             producto.setImagen(nombreImagen);
-        }else{
-            if (file.isEmpty()) {
-                Producto p= productoService.get(producto.getId()).get();
-                producto.setImagen(p.getImagen());
-            } else {
-                String imagen= upload.saveImage(file);
-                producto.setImagen(imagen);
-            }
         }
-        
         productoService.save(producto);
         return "redirect:/productos";
     }
-    
+
     @GetMapping("/edit/{id}")
-    public String edit(@PathVariable Integer id, Model model){
+    public String edit(@PathVariable Integer id, Model model) {
         try {
-            Producto producto= productoService.findById(id);
+            Producto producto = productoService.findById(id);
             LOGGER.info("Producto buscado: {} ", producto);
             model.addAttribute("producto", producto);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-            return "/productos/edit";
+        return "/productos/edit";
     }
-    
+
     @PostMapping("/update")
-    public String update(Producto producto){
+    public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+        Producto p = productoService.get(producto.getId()).get();
+        
+        if (file.isEmpty()) {
+            producto.setImagen(p.getImagen());
+        } else {
+            //cuando se modifica la imagen
+            //eliminar cuando no sea la imagen por defecto
+            if (!p.getImagen().equals("default.jpg")){
+                upload.delete(p.getImagen());
+            }
+            String imagen = upload.saveImage(file);
+            producto.setImagen(imagen);
+        }
+        producto.setUsuario(p.getUsuario());
         productoService.update(producto);
         return "redirect:/productos";
     }
-    
+
     @GetMapping("/delete/{id}")
-    public String delete (@PathVariable Integer id){
+    public String delete(@PathVariable Integer id) {
+        Producto producto = new Producto();
+        producto = productoService.get(id).get();
+        //eliminar cuando no sea la imagen por defecto
+        if (!producto.getImagen().equals("default.jpg")) {
+            upload.delete(producto.getImagen());
+        }
         productoService.delete(id);
         return "redirect:/productos";
     }
