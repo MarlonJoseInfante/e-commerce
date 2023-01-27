d65e41f9236195817ca08fff3a7f204b79c49dee
= <=>
2023-01-27 14:43:19 -0300

    Mostrar la vista de producto al usuario

--- src/main/java/com/practice/ecommerce/controller/HomeController.java
+++ src/main/java/com/practice/ecommerce/controller/HomeController.java
@@ -3,16 +3,20 @@
 package com.practice.ecommerce.controller;
 
 import com.practice.ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
     
+    private final Logger LOG= LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductoService productoService;
 
@@ -20,5 +24,11 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("productos", productoService.findAll());
         return "/usuario/home";
+    }
+    
+    @GetMapping("/productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        LOG.info("Id producto enviado como parametro {}", id);
+        return "/usuario/productohome";
     }
 }
