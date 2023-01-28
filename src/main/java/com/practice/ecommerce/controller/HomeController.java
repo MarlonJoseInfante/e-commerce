9fac87a488970c52128efd94cf28bdb30b33c07b
= <=>
2023-01-28 03:28:51 -0300

    Se implementaron los metodos de obtener la cantidad y busqueda del producto en Home Controller para añadir al carrito

--- src/main/java/com/practice/ecommerce/controller/HomeController.java
+++ src/main/java/com/practice/ecommerce/controller/HomeController.java
@@ -2,8 +2,12 @@
 
 package com.practice.ecommerce.controller;
 
+import com.practice.ecommerce.model.DetalleOrden;
+import com.practice.ecommerce.model.Orden;
 import com.practice.ecommerce.model.Producto;
 import com.practice.ecommerce.service.ProductoService;
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +17,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
     
     private final Logger LOG= LoggerFactory.getLogger(HomeController.class);
+    
     @Autowired
     private ProductoService productoService;
 
+    //para almacenar los detalles de la orden
+    List<DetalleOrden> detalles= new ArrayList();
+    
+    //datos de la orden
+    Orden orden= new Orden();
+    
     @GetMapping("")
     public String home(Model model){
         model.addAttribute("productos", productoService.findAll());
@@ -38,7 +50,14 @@ public class HomeController {
     }
     
     @PostMapping("/cart")
-    public String addCart(){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) throws Exception{
+        DetalleOrden detalleOrden= new DetalleOrden();
+        Producto producto= new Producto();
+        double sumaTotal=0;
+        producto= productoService.findById(id);
+        LOG.info("Producto añadido {}", producto);
+        LOG.info("Cantidad {}", cantidad);
+        
         return "/usuario/carrito";
     }
 }
